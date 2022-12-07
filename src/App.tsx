7629429dd7b346b558f558974c5c64a5543a5e5c
Andy Lee <rize1126jesse@gmail.com>
2022-12-07 18:43:01 +0900

    draw graphs

--- src/App.tsx
+++ src/App.tsx
@@ -1,112 +1,45 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import Highcharts from "highcharts";
+import Highcharts, { SeriesLineOptions } from "highcharts";
 import HighchartsReact from "highcharts-react-official";
-
-const options = {
-  title: {
-    text: "人口増加率",
-  },
-  yAxis: {
-    title: {
-      text: "Value",
-    },
-  },
-  xAxis: {
-    accessibility: {
-      rangeDescription: "Range: 1980 to 2020",
-    },
-  },
-  legend: {
-    layout: "vertical",
-    align: "right",
-    verticalAlign: "middle",
-  },
-  plotOptions: {
-    series: {
-      label: {
-        connectorAllowed: false,
-      },
-      pointInterval: 5,
-      pointStart: 1965,
-    },
-  },
-  series: [
-    {
-      name: "Installation & Developers",
-      data: [
-        43934, 48656, 65165, 81827, 112143, 142383, 171533, 165174, 155157,
-        161454, 154610,
-      ],
-    },
-    {
-      name: "Manufacturing",
-      data: [
-        24916, 37941, 29742, 29851, 32490, 30282, 38121, 36885, 33726, 34243,
-        31050,
-      ],
-    },
-    {
-      name: "Sales & Distribution",
-      data: [
-        11744, 30000, 16005, 19771, 20185, 24377, 32147, 30912, 29243, 29213,
-        25663,
-      ],
-    },
-    {
-      name: "Operations & Maintenance",
-      data: [
-        null,
-        null,
-        null,
-        null,
-        null,
-        null,
-        null,
-        null,
-        11164,
-        11218,
-        10077,
-      ],
-    },
-    {
-      name: "Other",
-      data: [
-        21908, 5548, 8105, 11248, 8989, 11816, 18274, 17300, 13053, 11906,
-        10073,
-      ],
-    },
-  ],
-
-  responsive: {
-    rules: [
-      {
-        condition: {
-          maxWidth: 500,
-        },
-        chartOptions: {
-          legend: {
-            layout: "horizontal",
-            align: "center",
-            verticalAlign: "bottom",
-          },
-        },
-      },
-    ],
-  },
-};
 
 function App() {
   const [data, setData] = useState([]);
   const [data2, setData2] = useState([]);
   const [val, setVal] = useState(["選択値"]);
-  const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
-
-  // const state = {
-  //   selected: Array(47).fill(false),
-  //   prefectures: {},
-  //   series: [],
-  // };
+  const [save, setSave] = useState<any>([]);
+  const [opt, setOptions] = useState<Highcharts.Options>({
+    title: {
+      text: "人口増加率",
+    },
+    yAxis: {
+      title: {
+        text: "Value",
+      },
+    },
+    xAxis: {
+      accessibility: {
+        rangeDescription: "Range: 1980 to 2020",
+      },
+    },
+    legend: {
+      layout: "vertical",
+      align: "right",
+      verticalAlign: "middle",
+    },
+    plotOptions: {
+      series: {
+        label: {
+          connectorAllowed: false,
+        },
+        pointInterval: 5,
+        pointStart: 1965,
+      },
+      line: {},
+    },
+    series: [],
+  });
+  // const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
 
   const getApi = () => {
     fetch("https://opendata.resas-portal.go.jp/api/v1/prefectures", {
@@ -118,7 +51,7 @@ function App() {
     })
       .then((response) => response.json())
       .then((json) => {
-        console.log(json);
+        //console.log(json);
         setData(json.result);
       });
   };
@@ -130,14 +63,14 @@ function App() {
   const handleChange = (e: any) => {
     // change したのはいいとして、ON なのか OFF なのか判定する必要がある
     if (val.includes(e.target.value)) {
-      // console.log(setVal)
-
-      setVal(val.filter((item) => item !== e.target.value));
+      return setVal(val.filter((item) => item !== e.target.value));
     } else {
+      console.log(e.target.value);
       // そうでなければ ON と判断し、
       // イベント発行元を末尾に加えた配列を set し直す
       setVal([...val, e.target.value]);
       // state は直接は編集できない
+      console.log(e.target.id);
       // つまり val.push(e.target.value) はNG ❌
       // すでに含まれていれば OFF したと判断し、
       // イベント発行元を除いた配列を set し直す
@@ -150,15 +83,74 @@ function App() {
         .then((response) => response.json())
         .then((json) => {
           console.log(json);
-          setData2(json.result.data[0].data); //今日はここをいじれ
-          //   state({
-          //     selected:
-          //   })
-          for (let i = 0; i < data2.length; i++) {}
+          setData2(json.result.data[0].data);
+          const tmp: number[] = [];
+
+          for (let i = 0; i < data2.length; i++) {
+            //console.log(data2[i]["value"]);
+            tmp.push(data2[i]["value"]);
+          }
+
+          const select_series: SeriesLineOptions = {
+            type: "line",
+            name: e.target.id,
+            data: tmp,
+          };
+          //console.log(select_series);
+          // options.series!.push(select_series);
+          //console.log(options);
+
+          // var save = pushJSON.parse(JSON.stringify(save));
+          // save.push(new Date().toString());
+          // save.push(JSON.parse(JSON.stringify(select_series)));
+          setSave([...save, select_series]);
+
+          console.log(save);
+          setOptions((prevState) => ({
+            ...prevState,
+            series: save,
+          }));
         });
     }
   };
 
+  // const options: Highcharts.Options = {
+  //   title: {
+  //     text: "人口増加率",
+  //   },
+  //   yAxis: {
+  //     title: {
+  //       text: "Value",
+  //     },
+  //   },
+  //   xAxis: {
+  //     accessibility: {
+  //       rangeDescription: "Range: 1980 to 2020",
+  //     },
+  //   },
+  //   legend: {
+  //     layout: "vertical",
+  //     align: "right",
+  //     verticalAlign: "middle",
+  //   },
+  //   plotOptions: {
+  //     series: {
+  //       label: {
+  //         connectorAllowed: false,
+  //       },
+  //       pointInterval: 5,
+  //       pointStart: 1965,
+  //     },
+  //     line: {},
+  //   },
+  //   series: [],
+  // };
+
+  // useEffect(() => {
+  //   // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+  //   HighchartsReact;
+
+  // });
   return (
     <div className="App">
       <h1>{val.join(", ")}</h1>
@@ -168,6 +160,7 @@ function App() {
             {item.prefName}
             <input
               type="checkbox"
+              id={item.prefName}
               value={item.prefCode}
               onChange={handleChange}
               //checked={val.includes(item.prefCode)}
@@ -175,11 +168,10 @@ function App() {
           </label>
         </div>
       ))}
-
       <HighchartsReact
         highcharts={Highcharts}
-        options={options}
-        ref={chartComponentRef}
+        options={opt}
+        // ref={chartComponentRef}
       />
     </div>
   );
